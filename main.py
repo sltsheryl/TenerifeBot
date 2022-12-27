3adcbd08e467550e6571ee7ac80ff71b6fdfa944
sltsheryl <sltsheryl@gmail.com>
2022-12-28 03:02:11 +0800

    Add test bot

---
+++ main.py
@@ -0,0 +1,23 @@
+# This works for dm messages
+
+import discord
+import os
+
+client = discord.Client(intents=discord.Intents.default())
+
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  if message.content.startswith('$hello'):
+    await message.channel.send('Hello!')
+
+
+client.run(os.getenv('TOKEN'))
