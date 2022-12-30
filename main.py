a55f0ba40988abeb6568096faf99f19a9f78136c
sltsheryl <sltsheryl@gmail.com>
2022-12-31 01:05:11 +0800

    Add weather functionality

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
 import os
+from keep_alive import keep_alive
+from weather import get_weather
 
 intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -15,8 +18,15 @@ async def on_message(message):
   if message.author == client.user:
     return
 
-  if message.content.startswith('$hello'):
-    await message.channel.send('Hello!')
+  if message.content == '~hello':
+    await message.channel.send('Hello! Enter ~help to get the list of all the commands!')
+
+  if message.content.startswith('~weather'): 
+    city = message.content[slice(9, len(message.content))]
+    print(city)
+    result = get_weather(city)
+    await message.channel.send(embed=result)
 
 
+keep_alive()
 client.run(os.getenv('TOKEN'))
